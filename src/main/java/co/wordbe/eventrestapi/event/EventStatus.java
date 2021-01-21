79ec091b64b6d6358ab76c75c0d884269368718e
Wordbe <seonghojin3@gmail.com>
2021-01-21 14:56:00 +0900

    210121 event domain create

---
+++ src/main/java/co/wordbe/eventrestapi/event/EventStatus.java
@@ -0,0 +1,7 @@
+package co.wordbe.eventrestapi.event;
+
+public enum EventStatus {
+    DRAFT,
+    PUBLISHED,
+    BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/co/wordbe/eventrestapi/event/EventTest.java
@@ -0,0 +1,33 @@
+package co.wordbe.eventrestapi.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Event REST API")
+                .description("REST API development")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        Event event = new Event();
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
